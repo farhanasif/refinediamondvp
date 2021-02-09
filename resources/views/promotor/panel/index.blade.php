9020ab2a0c9d57c0d5a0bda66c969fe68bddf0eb
Md Farhan Asif <f.asif2010@gmail.com>
2021-02-10 00:31:35 +0600

    update investment

--- resources/views/promotor/panel/index.blade.php
+++ resources/views/promotor/panel/index.blade.php
@@ -3,18 +3,83 @@
 @section('dashobard_title', 'Investment')
 @section('admin_content')
 
-<section class="content-header">
-  <div class="container-fluid">
+<!-- Content Header (Page header) -->
+<div class="content-header">
+    <div class="container-fluid">
     <div class="row mb-2">
-      <div class="col-sm-12">
-        <marquee style="color: red;">Welcome To Investment & Share Panel</marquee>
-      </div>
-    </div>
-  </div><!-- /.container-fluid -->
-</section>
+        <div class="col-sm-6">
+        <h1 class="m-0">Investment & Share Panel</h1>
+        </div><!-- /.col -->
+    </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+</div>
+<!-- /.content-header -->
 
 <section class="content">
   <div class="container-fluid">
+    <!-- Small boxes (Stat box) -->
+    <div class="row">
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-info">
+              <div class="inner">
+                <h3>1</h3>
+                <p>KING PANEL</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-person"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-success">
+              <div class="inner">
+                <h3>0</h3>
+
+                <p>PRINCE PANEL</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-person-add"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-danger">
+              <div class="inner">
+                <h3>0</h3>
+
+                <p>ROYAL PANEL</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-person-add"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-warning">
+              <div class="inner">
+                <h3>0</h3>
+
+                <p>GOLD PANEL</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-person-add"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+        </div>
+        <!-- /.row -->
     <div class="card card-default">
 
       <div class="card-header">
@@ -22,7 +87,11 @@
       </div>
       <!-- /.card-header -->
       <div class="card-body">
-
+            <div class="row">
+                <div class="col-3">
+                    <button type="button" class="btn btn-block bg-gradient-primary">Add a new panel user</button>
+                </div>
+            </div>
       </div>
       <!-- /.card-body -->
     </div>
